0385cd0e97d9996fcb09399a291b608eea7399f2
Suraj Wate <suraj.wate@gmail.com>
2023-10-14 21:51:03 +0100

    create a dummy data and pass it to home template

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,8 +1,26 @@
 from django.shortcuts import render
 
+posts = [
+    {
+        'author': 'Suraj',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'October 14, 2023'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'October 15, 2023'
+    }
+]
+
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html')
