4448e3bef0441d595b32f527bb7eaac686486705
Suraj Wate <suraj.wate@gmail.com>
2023-10-15 02:01:14 +0100

    deleted temporary posts

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,21 +1,6 @@
 from django.shortcuts import render
 from .models import Post
 
-posts = [
-    {
-        'author': 'Suraj',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'October 14, 2023'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'October 15, 2023'
-    }
-]
-
 
 def home(request):
     context = {
