1c2605c0962e886320d008ff9d9d5fde660d1f79
Suraj Wate <suraj.wate@gmail.com>
2023-10-18 18:40:15 +0100

    Update User Profile Fixes #15

--- django_project/users/forms.py
+++ django_project/users/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 
 class UserRegisterForm(UserCreationForm):
@@ -9,3 +10,17 @@ class UserRegisterForm(UserCreationForm):
     class Meta:  # gives us a nested namespace for configurations and keeps the configurations in one place
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+
+
+class UserUpdateForm(forms.ModelForm):  # allows us to update our username and email
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+
+class ProfileUpdateForm(forms.ModelForm):  # allows us to update our profile image
+    class Meta:
+        model = Profile
+        fields = ['image']
--- django_project/users/views.py
+++ django_project/users/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
-from .forms import UserRegisterForm
+from .forms import UserRegisterForm, UserUpdateForm, ProfileUpdateForm
 
 def register(request):
     if request.method == 'POST':
@@ -17,4 +17,11 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html')
+    u_form = UserUpdateForm()  # instance=request.user is to populate the form with the current user info
+    p_form = ProfileUpdateForm()  # instance=request.user.profile is to populate the form with the current user info
+
+    context = {
+        'u_form': u_form,
+        'p_form': p_form
+    }
+    return render(request, 'users/profile.html', context)
