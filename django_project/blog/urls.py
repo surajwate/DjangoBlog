9821676da4090afc54a28339320842507cb1fe2f
Suraj Wate <suraj.wate@gmail.com>
2023-10-20 10:58:02 +0100

    delete post

--- django_project/blog/urls.py
+++ django_project/blog/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from .views import (PostListView, 
                     PostDetailView, 
                     PostCreateView,
-                    PostUpdateView
+                    PostUpdateView,
+                    PostDeleteView,
                     )
 from . import views
 
@@ -11,5 +12,6 @@ urlpatterns = [
     path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('post/new/',PostCreateView.as_view(), name='post-create'),
     path('post/<int:pk>/update/',PostUpdateView.as_view(), name='post-update'),
+    path('post/<int:pk>/delete/',PostDeleteView.as_view(), name='post-delete'),
     path('about/', views.about, name='blog-about'),
 ]
--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -4,7 +4,8 @@ from django.views.generic import (
     ListView, 
     DetailView, 
     CreateView,
-    UpdateView
+    UpdateView,
+    DeleteView
     )
 from .models import Post
 
@@ -51,6 +52,17 @@ class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
             return True
         return False
 
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    success_url = '/blog' # redirect to home page after deleting a post
+
+    def test_func(self) -> bool | None:
+        # return super().test_func() and self.request.user == self.get_object().author
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 def about(request):
     return render(request, 'blog/about.html', {'title':'About'})
