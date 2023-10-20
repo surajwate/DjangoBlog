959043b9516e773a38344f9900c66457411ed132
Suraj Wate <suraj.wate@gmail.com>
2023-10-20 10:14:49 +0100

    update the post

--- django_project/blog/views.py
+++ django_project/blog/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
-from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, DetailView, CreateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView, 
+    DetailView, 
+    CreateView,
+    UpdateView
+    )
 from .models import Post
 
 
@@ -28,6 +33,23 @@ class PostCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.author = self.request.user
         return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    fields = ['title', 'content']
+
+    # This method will ensure that only the author of a post can update it
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+    
+    def test_func(self) -> bool | None:
+        # return super().test_func() and self.request.user == self.get_object().author
+        post = self.get_object()
+        if self.request.user == post.author:
+            return True
+        return False
 
 
 def about(request):
