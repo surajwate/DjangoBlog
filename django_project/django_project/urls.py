e07cd58f75c5059bc072cffb5f45c22a8d8755c1
Suraj Wate <suraj.wate@gmail.com>
2023-10-21 17:19:29 +0100

    password reset confirm - view, url and template

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -33,6 +33,9 @@ urlpatterns = [
     path('password-reset/done/',
         auth_views.PasswordResetDoneView.as_view(template_name='users/password_reset_done.html'), 
         name='password_reset_done'),
+    path('password-reset-confirm/<uidb64>/<token>/',
+        auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'), 
+        name='password_reset_confirm'),
     path('blog/', include('blog.urls')),
 ]
 
