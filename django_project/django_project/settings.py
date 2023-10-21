8092b3969cbfd93140573c692a9324b231a079b8
Suraj Wate <suraj.wate@gmail.com>
2023-10-21 17:54:03 +0100

    email server setup

--- django_project/django_project/settings.py
+++ django_project/django_project/settings.py
@@ -139,3 +139,10 @@ CRISPY_TEMPLATE_PACK = 'bootstrap5'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mailgun.org'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_PASS')
