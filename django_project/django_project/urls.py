ac54cb08b00ef49cf14e9220a26c09c36ec23db6
Suraj Wate <suraj.wate@gmail.com>
2023-10-22 06:51:50 +0100

    blog app into root location

--- django_project/django_project/urls.py
+++ django_project/django_project/urls.py
@@ -40,6 +40,7 @@ urlpatterns = [
         auth_views.PasswordResetCompleteView.as_view(template_name='users/password_reset_complete.html'), 
         name='password_reset_complete'),
     path('blog/', include('blog.urls')),
+    path('', include('blog.urls')),
 ]
 
 
